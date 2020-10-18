0b14511bf5b9a77548dd8c5317ecd3432f5a3af0
Juliën Schelfhout <crosinor@gmail.com>
2020-10-18 21:42:17 +0200

    Toevoeging fetch_bottle en tabel iets aangepast (NFC tag obv UID kolom (text)

--- whine_base.py
+++ whine_base.py
@@ -6,20 +6,27 @@ from whine_classes import RoseWhine
 
 #Imports from whine_db_SDK
 from whine_DB_SDK import create_table
+from whine_DB_SDK import recreate_table
 from whine_DB_SDK import add_whine
-from whine_DB_SDK import fetch_results
+from whine_DB_SDK import fetch_all_results
+from whine_DB_SDK import fetch_bottle
 from whine_DB_SDK import delete_selected
 from whine_DB_SDK import clear_results
 
 import datetime
 
-fles_1 = WhineBottle(1, 'generieke wijn', 'merlot', 2014)
-fles_2 = RedWhine(2, 'Test rode wijn', 'Shiraz', 2015, ['Shiraz', 'Merlot'])
-fles_3 = WhiteWhine(3, 'Witte wijn','Chardonnay',2016,'D',['Chardonnay', 'Sauvignon Blanc'])
-fles_4 = RoseWhine(4, 'Rose wijn', 'Semillion', 2017, 'S',['Semillion','Chardonnay'])
+#fles_1 = WhineBottle(	'vb1', 'generieke wijn', 	'merlot'		,2014)
+#fles_2 = RedWhine(		'vb2', 'Test rode wijn', 	'Shiraz'		,2015, 	['Shiraz', 'Merlot'])
+#fles_3 = WhiteWhine(	'vb3', 'Witte wijn',		'Chardonnay'	,2016,	'D'						,['Chardonnay', 'Sauvignon Blanc'])
+#fles_4 = RoseWhine(		'vb4', 'Rose wijn', 		'Semillion'		,2017, 	'S'						,['Semillion','Chardonnay'])
 
-create_table()
+#recreate_table() #-- voor testen aanmaken nieuwe db
 
-add_whine(fles_2.ID, fles_2.name, fles_2.main_grape, fles_2.year, fles_2.list_grape_mix(), datetime.date.today())
+#create_table() -- voor aanmaken nieuwe tabel (bij lege db)
 
-print(delete_selected(2))
+#fles = RedWhine(		'vb1', 'Eerste test rode wijn', 	'Shiraz'		,2018, 	['Shiraz', 'Merlot'])
+#fles = RedWhine(		'vb2', 'Nog een test rode wijn', 	'Shiraz'		,2018, 	['Shiraz', 'Merlot'])
+#add_whine(fles.UID, fles.name, fles.main_grape, fles.year, fles.list_grape_mix(), datetime.date.today())
+
+# fles scannen
+fetch_bottle("vb1")
