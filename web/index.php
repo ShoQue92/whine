bff8d9da0a9359947472022eca8a4e74ad3b54cd
Juliën Schelfhout <crosinor@gmail.com>
2020-10-21 00:13:59 +0200

    v1 web frontend

---
+++ web/index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<link rel="stylesheet" href="style.css">
+<script type="text/javascript" src="code.js"></script>
+</head>
+<body>
+<?php
+$nieuweflescsvpad = "workdir/nieuwefles.csv";
+
+$filevalid=false;
+if( file_exists($nieuweflescsvpad)){
+	$fp = file($nieuweflescsvpad);
+	if(count($fp) == 2){
+		$bestand = file($nieuweflescsvpad,FILE_SKIP_EMPTY_LINES); 
+		$bestandinhoud = array_map("str_getcsv",$bestand, array_fill(0, count($bestand), ';'));
+		
+		if($bestandinhoud[1][0] == "registered"){	
+			$filevalid=true;
+		}
+	}
+}
+
+if($filevalid){
+?>
+<h3 class="ui-bar ui-bar-a ui-corner-all">Nieuwe wijnfles gescand, klik hieronder om toe te voegen.</h3>
+<a href="nieuwefles.php" class="ui-btn ui-shadow" data-transition="pop">Toevoegen Nieuwe Fles</a>
+<?php
+}
+else{
+?>
+<div>Bestand '<?php echo $nieuweflescsvpad; ?>' niet gevonden, invalid of al verwerkt..</div>
+<?php
+}
+?>
+
+</body>
+<html>
---
+++ web/nieuwefles.php
@@ -0,0 +1,67 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+<script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<link rel="stylesheet" href="style.css">
+<script type="text/javascript" src="code.js"></script>
+</head>
+<body>
+<a href="index.php" class="ui-btn ui-shadow" data-transition="pop">Terug</a>
+
+<?php
+
+$nieuweflescsvpad = "workdir/nieuwefles.csv";
+$filecorrect = false;
+
+if( file_exists($nieuweflescsvpad)){
+	$fp = file($nieuweflescsvpad);
+	if(count($fp) == 2){
+		$filecorrect = true;
+		# file bevat 2 regels, verdergaan
+		$bestand = file($nieuweflescsvpad,FILE_SKIP_EMPTY_LINES); 
+		$bestandinhoud = array_map("str_getcsv",$bestand, array_fill(0, count($bestand), ';'));
+	}
+	else{
+		# file bestaat maar bevat geen 2 regels
+		echo "bestand " . $nieuweflescsvpad . " heeft geen 2 regels.";
+	}
+}
+else {
+	# file bestaat niet, lege pagina
+	echo "bestand " . $nieuweflescsvpad . " bestaat niet.";
+}
+?>
+<h3 class="ui-bar ui-bar-a ui-corner-all">Toevoegen nieuwe wijnfles</h3>
+
+<div data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-icon="null" data-iconpos="null" data-theme="b" class="ui-btn ui-btn-up-b ui-shadow ui-btn-corner-all ui-submit" aria-disabled="false">
+<form name="wijnflestoevoegen" action="actie.php" method="post">
+	<input type="hidden" name="actie" value="toevoegenfles" />
+	<span class="ui-btn-inner ui-btn-corner-all">
+		<div data-role="fieldcontain">	
+			<?php
+			$disabledvelden = array("UID","date_in_fridge");
+			
+			$kolomnr=0;
+			foreach($bestandinhoud[0] as $header){
+				if($header != "status"){
+					if(in_array($header,$disabledvelden)){
+					?>
+					<input type="hidden" name="<?php echo $header; ?>" value="<?php echo $bestandinhoud[1][$kolomnr]; ?>" />
+					<?php
+					}
+					?>
+					<label for="<?php echo $header; ?>"><?php echo str_replace("_"," ",ucfirst($header)); ?></label>
+					<input <?php if(in_array($header,$disabledvelden)){echo "disabled=\"disabled\""; } ?> type="text" name="<?php echo $header; ?>" value="<?php echo $bestandinhoud[1][$kolomnr]; ?>" />
+					<?php
+					}
+			$kolomnr++;
+			}
+			?>
+		</div>	
+	</span>
+	<button type="submit" data-theme="b" name="submit" value="submit" class="ui-btn-hidden" aria-disabled="false">Submit</button>
+</form>	
+</div>
+</body>
+<html>
