482186f11de09f960c3d3231094a34cccfc7c9c2
Joerie Brugts <joerie@brugts.nl>
2020-11-07 22:55:52 +0100

    melding gecorrigeerd

--- whine_DB_SDK.py
+++ whine_DB_SDK.py
@@ -105,9 +105,6 @@ def export_bottle_properties_csv(tgt_file,tgt_dir):
             print("Kon bestand " +tgt_file+ "niet aanmaken...")
 
 def check_bottle_existance(UID):
-    message = "Er wordt gekeken of fles {} reeds bestaat".format(UID)
-    print(message)
-    time.sleep(1)
     c.execute("SELECT UID, name, main_grape, year, date_in_fridge FROM whine_bottles WHERE UID='"+UID+"'")
     data = c.fetchone()
     if data:
