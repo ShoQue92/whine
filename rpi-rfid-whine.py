23a3aaabab840eac7b03cda25c83747f6ee6d4da
Joerie Brugts <joerie@brugts.nl>
2020-12-09 23:16:29 +0100

    probeersel met functie die zichzelf weer roept

--- rpi-rfid-whine.py
+++ rpi-rfid-whine.py
@@ -22,24 +22,29 @@ from whine_base import create_init_file
 import datetime
 import time
 
-tgt_file = "new_bottle.csv"
-tgt_dir = "/var/www/html/interface_files"
+def read_rfid():
+        tgt_file = "new_bottle.csv"
+        tgt_dir = "/var/www/html/interface_files"
 
-reader = SimpleMFRC522()
-print('RFID lezer actief, plaats een tag voor de lezer...'.center(100,'='))
-time.sleep(1)
-try:
-        id, text = reader.read()
-        fles = WhineBottle(str(id), '', '', '', '')
-        #Als de gescande tag reeds bestaat, laat de data zien, anders een nieuwe toevoegen.
-        if check_bottle_existance(fles.UID):
-                fetch_bottle(fles.UID)
-                print('EINDE data opgehaald.'.center(100,'='))
-        else:
-                add_whine(fles.UID, fles.name, fles.main_grape, fles.year, fles.properties, datetime.datetime.now())
-                print("Interface bestand {} wordt aangemaakt op locatie {}".format(tgt_file, tgt_dir))
-                create_init_file(tgt_file, tgt_dir, fles.UID)
-                print('EINDE fles toegevoegd.'.center(100,'='))
-finally:
-        GPIO.cleanup()
+        reader = SimpleMFRC522()
+        print('RFID lezer actief, plaats een tag voor de lezer...'.center(100,'='))
+        time.sleep(1)
 
+        try:
+                id, text = reader.read()
+                fles = WhineBottle(str(id), '', '', '', '')
+                #Als de gescande tag reeds bestaat, laat de data zien, anders een nieuwe toevoegen.
+                if check_bottle_existance(fles.UID):
+                        fetch_bottle(fles.UID)
+                        print('EINDE data opgehaald.'.center(100,'='))
+                        read_rfid()
+                else:
+                        add_whine(fles.UID, fles.name, fles.main_grape, fles.year, fles.properties, datetime.datetime.now())
+                        print("Interface bestand {} wordt aangemaakt op locatie {}".format(tgt_file, tgt_dir))
+                        create_init_file(tgt_file, tgt_dir, fles.UID)
+                        print('EINDE fles toegevoegd.'.center(100,'='))
+                        read_rfid()
+        finally:
+                GPIO.cleanup()
+
+read_rfid()
