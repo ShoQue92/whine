51c1590616fe0d7dbae3d9ef1190a4c910b05baf
Joerie Brugts <joerie@brugts.nl>
2020-12-11 19:47:16 +0100

    clear_db als front-end_action toegeveogd

--- front_end_actions.py
+++ front_end_actions.py
@@ -1,5 +1,6 @@
 from whine_DB_SDK import update_whine
 from whine_DB_SDK import export_bottle_properties_csv
+from whine_DB_SDK import clear_results
 from whine_base import process_return_file
 from whine_base import process_bottle_property_return_file
 from sys import argv
@@ -14,5 +15,7 @@ elif actie == 'export_bottle_properties':
        export_bottle_properties_csv(file,tgt_dir)
 elif actie == 'process_bottle_properties':
        process_bottle_property_return_file(file, tgt_dir)
+elif actie == "clear_db":
+       clear_results()
 else:
       print('Geen juiste actie opgegeven')
