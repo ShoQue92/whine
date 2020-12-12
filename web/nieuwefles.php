02ad9c944eecc293290774017e6bfd873eb5b1f9
Juliën Schelfhout <crosinor@gmail.com>
2020-12-12 16:44:11 +0100

    Vele front-end aanpassingen tbv bruikbaarheid

--- web/nieuwefles.php
+++ web/nieuwefles.php
@@ -4,32 +4,18 @@
 </head>
 <body>
 
-<?php paginaheader("Wijnkoeler"); ?>
+<?php paginaheader("Nieuwe fles"); ?>
 
 <a href="index.php" class="ui-btn ui-shadow" data-transition="pop">Terug</a>
 
 <?php
 
-$nieuweflescsvpad = "interface_files/intf_init_bottle.csv";
-$filecorrect = false;
+$bestandinhoud = read_intf_init_bottle_content(false);
 
-if( file_exists($nieuweflescsvpad)){
-	$fp = file($nieuweflescsvpad);
-	if(count($fp) == 2){
-		$filecorrect = true;
-		# file bevat 2 regels, verdergaan
-		$bestand = file($nieuweflescsvpad,FILE_SKIP_EMPTY_LINES); 
-		$bestandinhoud = array_map("str_getcsv",$bestand, array_fill(0, count($bestand), ';'));
-	}
-	else{
-		# file bestaat maar bevat geen 2 regels
-		echo "bestand " . $nieuweflescsvpad . " heeft geen 2 regels.";
-	}
-}
-else {
-	# file bestaat niet, lege pagina
-	echo "bestand " . $nieuweflescsvpad . " bestaat niet.";
-}
+$db = new SQLite3('db/whine_inventory.db');
+$querygrapesoorten = $db->prepare("SELECT grape FROM grapes order by grape asc;");
+$resultaatgrapesoorten = $querygrapesoorten->execute();
+
 ?>
 <h3 class="ui-bar ui-bar-a ui-corner-all" style="text-align:center">Toevoegen nieuwe wijnfles</h3>
 
@@ -37,30 +23,64 @@ else {
 <form name="wijnflestoevoegen" action="actie.php" method="post" autocomplete="off">
 	<input type="hidden" name="actie" value="toevoegenfles" />
 	<span class="ui-btn-inner ui-btn-corner-all">
-		<div data-role="fieldcontain">	
+		<div data-role="fieldcontain">
+		
 			<?php
 			$disabledvelden = array("UID","date_in_fridge");
 			
 			$kolomnr=0;
 			foreach($bestandinhoud[0] as $header){
-				if($header != "status" && $header != "type"){
+				if($header != "status" && $header != "type" && $header != "year" && $header != "main_grape"){
 					if(in_array($header,$disabledvelden)){
 					?>
 					<input type="hidden" name="<?php echo $header; ?>" value="<?php echo $bestandinhoud[1][$kolomnr]; ?>" />
 					<?php
 					}
-					
 					?>
 					<label for="<?php echo $header; ?>"><?php echo str_replace("_"," ",ucfirst($header)); ?></label>
-					<input required <?php if(in_array($header,$disabledvelden)){echo "disabled=\"disabled\""; } ?> type="text" name="<?php echo $header; ?>" value="<?php echo $bestandinhoud[1][$kolomnr]; ?>" />
+					<input required <?php if(in_array($header,$disabledvelden)){echo "disabled=\"disabled\""; } ?> type="text" name="<?php echo $header; ?>" value="<?php if($header == "date_in_fridge"){echo substr($bestandinhoud[1][$kolomnr],0,16); } else {echo $bestandinhoud[1][$kolomnr]; } ?>" style="text-align:center" />
 					<?php
 				}
+					elseif($header == "main_grape"){
+					?>
+					
+					    <label for="select-custom-1">Basis druif:</label>
+					    <select name="main_grape" id="select-custom-1" data-native-menu="false" data-mini="true">
+							<option value="Kies" data-placeholder="true">Kies...</option>
+							<?php
+							while ($row = $resultaatgrapesoorten->fetchArray()) {
+							?>
+					        <option value="<?php echo $row['grape'];; ?>"><?php echo $row['grape']; ?></option>
+							<?php
+							}
+							?>
+					    </select>
+					
+					<?php
+					}
+					elseif($header == "year"){
+					?>
+					
+					    <label for="select-custom-2">Jaar:</label>
+					    <select name="year" id="select-custom-2" data-native-menu="false" data-mini="true">
+							<option value="Kies" data-placeholder="true">Kies...</option>
+							<?php
+							for($x = date("Y"); $x >= date("Y")-15; $x--){
+							?>
+					        <option value="<?php echo $x; ?>"><?php echo $x; ?></option>
+							<?php
+							}
+							?>
+					    </select>
+					
+					<?php
+					}
+				
 					elseif($header == "type"){
 					?>
 					
-					
-					    <label for="select-custom-1">Fles type:</label>
-					    <select name="type" id="select-custom-1" data-native-menu="false" data-mini="true">
+					    <label for="select-custom-3">Soort wijn:</label>
+					    <select name="type" id="select-custom-3" data-native-menu="false" data-mini="true">
 							<option value="Kies" data-placeholder="true">Kies...</option>
 					        <option value="Rood">Rood</option>
 					        <option value="Wit">Wit</option>
@@ -81,8 +101,6 @@ else {
 </div>
 
 <?php require 'paginaeind.php'; ?>
-
-<?php require 'menu.php'; ?>
 
 </body>
 </html>
