c0be31bc22a3bceee7a1e9a0e6a302ef4d4a8214
Joerie Brugts <joerie@brugts.nl>
2020-12-22 20:29:57 +0100

    Functionaliteit gemaakt voor verwijderen specifieke fles

--- front_end_actions.py
+++ front_end_actions.py
@@ -1,10 +1,8 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 from whine_DB_SDK import update_whine
 from whine_DB_SDK import export_bottle_properties_csv
 from whine_DB_SDK import clear_results
 from whine_DB_SDK import recreate_table
+from whine_DB_SDK import delete_whine
 from whine_base import process_return_file
 from whine_base import process_bottle_property_return_file
 from sys import argv
@@ -17,32 +15,19 @@ front_end_actions.py <actie*> <file> <dir>
 Input argument <actie> is veprlicht en altijd als eerste, de rest is optioneel, maar wel verplicht voor een aantal acties en verschillend per functie.
 '''
 
-# defineer alle commando's in deze dictionary
-commands = {
-       "clear_db": clear_results,
-       "process_bottle": process_return_file,
-       "export_bottle_properties": export_bottle_properties_csv,
-       "process_bottle_properties": process_bottle_property_return_file,
-       "update_whine": update_whine,
-       "recreate_db": recreate_table
-}
-
-# bestandsnaam negeren
-argv.pop(0)
-
-if not argv:
-       print("Geen commando meegegeven!")
-       exit(1)
-
-# eerste cmd argument is altijd de command
-received_command = argv.pop(0)
-# match command naar functie met de dictionary, bestaat deze niet dan krijgen we None terug
-matched_command = commands.get(received_command)
-
-#check of command bestaat, als dat het geval is roep deze aan met de rest van de argumenten
-if not matched_command:
-       print("Geen geldig commando gevonden.")
-       exit(1)
-
-matched_command(*argv)
-exit(0)
+if argv[1] == "clear_db":
+       clear_results()
+elif argv[1] == 'process_bottle':
+      process_return_file(argv[2], argv[3]) 
+elif argv[1] == "export_bottle_properties":
+       export_bottle_properties_csv(argv[2], argv[3]) 
+elif argv[1] == "process_bottle_properties":
+       process_bottle_property_return_file(argv[2], argv[3])
+elif argv[1] == "update_whine":
+       update_whine(argv[2], argv[3], argv[4], argv[5], argv[6])
+elif argv[1] == "recreate_db":
+       recreate_table()
+elif argv[1] == "delete_bottle":
+       delete_whine(argv[2]) 
+else:
+      print('Geen juiste actie opgegeven!')
