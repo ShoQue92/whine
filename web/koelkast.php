4f0cba3ee12af02987d7366b89b91fb0ec71ca1b
Juliën Schelfhout <crosinor@gmail.com>
2020-12-29 22:16:21 +0100

    slider width 100procent

--- web/koelkast.php
+++ web/koelkast.php
@@ -150,7 +150,7 @@ function getwijnsoort($wijnsoort, $resultaat){
 ?>
 
 <div id="actualofhistory" class="ui-bar ui-bar-a ui-corner-all">
-	<div class="containing-element" style="margin: 0 auto;display:block">
+	<div class="containing-element">
 	<select name="flip-min" id="flip-min" data-role="slider">
 		<option value="off">Nu in de koelkast</option>
 		<option value="on">Al opgedronken</option>
