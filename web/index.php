5b0bad76aee7e61af1493505b525682de22836eb
Juliën Schelfhout <crosinor@gmail.com>
2021-01-02 19:41:49 +0100

    test

--- web/index.php
+++ web/index.php
@@ -17,7 +17,7 @@ require 'functies.php';
 </div>
 
 <div id="temp_<?php echo $randomnummer; ?>" class="temp">Temperatuur laden..</div>
-<div class="momenteelindekoelkast"><p>Momenteel in de koelkast</p></div>
+<div class="momenteelindekoelkast" style="width:100%;text-align:center;height:40px;line-height:40px;"><p>Momenteel in de koelkast</p></div>
 <div style="width:100%">
 <a href="koelkast.php"><img src="images/koelkast.png" style="width:100%" /></a>
 </div>
