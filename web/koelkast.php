07f7b1b01b48de19949b6a0863917dcb8833afab
Juliën Schelfhout <crosinor@gmail.com>
2021-01-02 10:40:43 +0100

    test pagechange

--- web/koelkast.php
+++ web/koelkast.php
@@ -19,7 +19,7 @@ $(".koelkasttype").on("slidestop", function(){
 <script>
 
 //$(document).on("pagecreate", "#page", function(){
-$(window).load(function() {	
+jQuery( ".selector" ).on( "pagechange",function() {	
     $(".animateMe .ui-collapsible-heading-toggle").on("click", function (e) { 
         var current = $(this).closest(".ui-collapsible");             
         if (current.hasClass("ui-collapsible-collapsed")) {
