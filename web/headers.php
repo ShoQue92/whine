06c8b9a4fa84b56cb131d8ed0c4af36ea93ea058
Juliën Schelfhout <crosinor@gmail.com>
2021-01-10 22:56:29 +0100

    typos

--- web/headers.php
+++ web/headers.php
@@ -3,9 +3,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
-<script>
-$(document).bind("mobileinit", function(){
-  ajaxEnabled: false
+<script type="text/javascript">
+$(document).bind("mobileinit", function () {
+    $.mobile.ajaxEnabled = false;
 });
 </script>
 <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
