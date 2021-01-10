23d85311345cbd87760fc0b405d0605755ef1bb9
Juliën Schelfhout <crosinor@gmail.com>
2021-01-10 22:55:06 +0100

    disable ajax

--- web/headers.php
+++ web/headers.php
@@ -3,6 +3,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1"> 
 <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+<script>
+$(document).bind("mobileinit", function(){
+  ajaxEnabled: false
+});
+</script>
 <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <link rel="stylesheet" href="style.css">
 <script type="text/javascript" src="code.js"></script>
@@ -11,8 +16,3 @@ header("Cache-Control: no-cache, no-store, must-revalidate"); // HTTP 1.1.
 header("Pragma: no-cache"); // HTTP 1.0.
 header("Expires: 0"); // Proxies.
 ?>
-<script>
-$(document).bind("mobileinit", function(){
-  ajaxEnabled: false
-});
-</script>
