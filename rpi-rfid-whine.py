ebddb5baba58ff3067908bcde6ab37b2619197e2
ShoQue92 <72988098+ShoQue92@users.noreply.github.com>
2021-03-27 21:46:55 +0100

    beep() functie

--- rpi-rfid-whine.py
+++ rpi-rfid-whine.py
@@ -38,10 +38,21 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 interface_path_env =  os.environ.get("INTF_ENV")
 
+def beep():
+        buzzer=23 #GPIO pin 23 wordt gebruikt voor de beep speaker
+        try:
+                GPIO.setmode(GPIO.BCM)
+                GPIO.setup(buzzer,GPIO.OUT)
+                GPIO.output(buzzer,GPIO.HIGH)
+                time.sleep(0.5)
+                GPIO.output(buzzer,GPIO.LOW)
+        finally:
+                GPIO.cleanup()
+
 def read_rfid():
         tgt_file = "intf_init_bottle.csv"
         tgt_dir = interface_path_env
-        buzzer=23 #GPIO pin 23 wordt gebruikt voor de beep speaker
+        
         reader = SimpleMFRC522()
         print('...RFID lezer actief, plaats een tag voor de lezer...'.center(100,'='))
         time.sleep(1)
@@ -49,9 +60,7 @@ def read_rfid():
         try:
                 id, text = reader.read()
                 if id:
-                        GPIO.output(buzzer,GPIO.HIGH)
-                        time.sleep(0.5)
-                        GPIO.output(buzzer,GPIO.LOW)
+                        beep() #Als het lezen van de RFID is gelukt, roep de beep() functie aan.
                 fles = WhineBottle(str(id), '', '', '', '', '')
                 #Als de gescande tag reeds bestaat, laat de data zien, anders een nieuwe toevoegen.
                 if check_bottle_existance(fles.UID):
